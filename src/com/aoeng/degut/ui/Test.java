21d53cca04475589c94da1b7893aaf8a8d447324
tianshan20081 <zhangshch2008@gmail.com>
2013-08-16 15:39:09 +0800

    Text Class

---
+++ src/com/aoeng/degut/ui/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.aoeng.degut.ui;
+
+/**
+ * @author [Aoeng Zhang] @datatime Aug 16, 2013:3:35:49 PM
+ * @Email [zhangshch2008@gmail.com] Aug 16, 2013
+ */
+public class Test {
+
+}
